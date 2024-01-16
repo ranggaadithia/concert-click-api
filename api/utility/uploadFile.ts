78de965f21af2ff69e971c9d9a9bdc2ddd4292cc
Rangga Adithia <rangga.adithia26@gmail.com>
2024-01-16 22:07:36 +0800

    fix: path

--- api/utility/uploadFile.ts
+++ api/utility/uploadFile.ts
@@ -1,7 +1,8 @@
 import multer from "multer";
+import path from "path";
 const storage = multer.diskStorage({
  destination: function (req, file, cb) {
-   cb(null, process.cwd()+ '/banners')
+   cb(null, path.join(process.cwd(), 'banners'))
  },
  filename: function (req, file, cb) {
    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
