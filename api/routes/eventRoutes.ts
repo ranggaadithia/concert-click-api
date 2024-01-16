6e09f0f59bda4a9b280b20389218955a32a1c605
Rangga Adithia <rangga.adithia26@gmail.com>
2024-01-16 21:25:16 +0800

    refactor: upload banner

--- api/routes/eventRoutes.ts
+++ api/routes/eventRoutes.ts
@@ -1,25 +1,14 @@
 import express from "express";
-import multer from "multer";
+import { upload } from "../utility/uploadFile";
 import { EventController } from "../controllers/eventController";
 import { checkAuthMiddleware } from "../middleware/checkAuthMiddleware";
 
 const eventController = new EventController();
 
 const routes = express.Router();
-const storage = multer.diskStorage({
- destination: function (req, file, cb) {
-   cb(null, 'banners')
- },
- filename: function (req, file, cb) {
-   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-   cb(null, file.fieldname+ '-' +uniqueSuffix + '-' + file.originalname)
- }
-})
-const upload = multer({storage})
 
 export const eventRouter = () => {
- routes.use('/', checkAuthMiddleware);
- routes.get('/', eventController.index);
+ routes.get('/', checkAuthMiddleware, eventController.index);
  routes.post('/', upload.single('banner'), checkAuthMiddleware,  eventController.create);
 
  return routes;
