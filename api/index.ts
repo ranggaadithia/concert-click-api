144ac00be81a1cfaac77c1a6735a617cd0483c64
Rangga Adithia <rangga.adithia26@gmail.com>
2024-01-23 18:59:43 +0800

    test webhook midtrans

--- api/index.ts
+++ api/index.ts
@@ -4,8 +4,16 @@ import { eventRouter } from './routes/eventRoutes';
 import { ticketRouter } from './routes/ticketRoutes';
 import { purchaserRouter } from './routes/purchaserRoutes';
 import { ticketPurchaseRoutes } from './routes/ticketPurchaseRoutes';
+const midtransClient = require('midtrans-client');
 
 const app = express();
+// Create Core API / Snap instance (both have shared `transactions` methods)
+let apiClient = new midtransClient.Snap({
+        isProduction : false,
+        serverKey : 'YOUR_SERVER_KEY',
+        clientKey : 'YOUR_CLIENT_KEY'
+    });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,7 +31,21 @@ app.use('/event', eventRouter());
 app.use('/ticket', ticketRouter());
 app.use('/purchaser', purchaserRouter());
 app.use('/buy', ticketPurchaseRoutes());
+app.get('/test', (req: Request, res: Response) => {
+  console.log('test application success to running');
+  res.send('test');
+});
 
+app.post('/notification', (req: Request, res: Response, notificationJson) => {
+  apiClient.transaction.notification(notificationJson)
+    .then((statusResponse: { order_id: any; transaction_status: any; fraud_status: any; })=>{
+        let orderId = statusResponse.order_id;
+        let transactionStatus = statusResponse.transaction_status;
+        let fraudStatus = statusResponse.fraud_status;
+
+        console.log(`Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`);
+    });
+});
 
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
